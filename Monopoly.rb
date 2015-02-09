541fe5c80dc81cf6d0c7135093d0ca1b1e0d5111
Darth-ATA <darthata@gmail.com>
2015-02-09 12:00:48 +0100

    He añadido la gestión a un método de la clase administrador, además ya he conseguido que se comunique de manera correcta ruby con MariaDB, tengo que conseguir que el flujo de mensajes de MariaDB se me muestre en el programa ruby

--- Monopoly.rb
+++ Monopoly.rb
@@ -2,54 +2,98 @@
 require 'mysql'
 
 class Administrador
-    #Inserción de un tablero en la base de datos
-    def insertarTablero(id_tablero)
-        con.query("INSTERT INTO Tablero(idTablero) VALUES('#{id_tablero}')")
-    end
-    def borrarTablero(id_tablero)
-        con.query("DELETE FROM Tablero")
-    end
-end
-begin
-    con = Mysql.new 'localhost', 'monopoly', 'monopoly', 'Monopoly'
-    
-    gestionando = "vamos"
-    subsistema = "vamos"
+    def initialize(nombre = "admin")
+        @nombre = nombre
+        #Son dos variables que controlan el menú
+        @gestionando = "OK"
+        @subsistema = "OK"
+        
+        #Conecta con la base de datos y nos da la versión que se está usando
+        @con = Mysql.new 'localhost', 'monopoly', 'monopoly', 'Monopoly'
+        
+        puts @con.get_server_info
+        @rs = @con.query 'SELECT VERSION()'
+        puts @rs.fetch_row 
 
-    puts "En caso de ser un Administrador escriba 1."
-    individuo = Integer(gets.chomp)
-    if(individuo == 1)
-        while(subsistema != "quit")
+    end
+    #Inserción de un tablero en la base de datos
+    def insertarTablero
+        #Crea la tabla tablero si no la tenemos ya en el sistema
+        @con.query("CREATE TABLE IF NOT EXISTS tablero(idTablero varchar(20) primary key, numeroCasillas int);")
+        puts 'Inserte el idTablero que desea agregar'
+        id_tablero = gets.chomp
+        @con.query("INSERT INTO tablero(idTablero) VALUES('#{id_tablero}');")
+    end
+    def borrarTablero
+        puts 'Inserte el idTablero que desea borrar'
+        id_tablero = gets.chomp
+        @con.query("DELETE FROM tablero where ('#{id_tablero}' = idTablero)")
+    end
+    def asociarCasilla
+        puts 'Inserte el idTablero que al que desea asociar casilla'
+        id_tablero = gets.chomp
+        puts 'Inserte el idCasilla que desea asociar'
+        id_casilla = gets.chomp
+        @con.query("INSERT INTO asociadas(idTablero,idCasilla) VALUES('#{id_tablero},#{id_casilla}")
+    end
+    def verTablero
+        puts 'Inserte el idTablero que desea ver'
+        id_tablero = gets.chomp
+        @con.query("Select * from tablero where idTablero = '#{id_tablero}'")
+    end
+    def gestion
+        while(@subsistema != "quit")
             puts "Bienvenido Administrador"
             puts "¿Qué desea gestionar? \n\t1- Tablero \n\t2- Casillas \n\t3- Tarjetas"
             print "Opcion: "
             gestionar = Integer(gets.chomp)
             if(gestionar == 1)
-                while gestionando != "quit" do
+                while @gestionando != 9 do
                     puts "Gestión de Tableros"
-                    puts "\t1- Insertar \n\t2- Borrar \n\t3- Asociar una casilla a un tablero"
+                    puts "\t1- Insertar \n\t2- Borrar \n\t3- Asociar una casilla a un tablero \n\t4- Ver \n\t9- Salir"
                     print "Opcion: "
-                    gestionando = gets.chomp
+                    @gestionando = Integer(gets.chomp)
+                    if(@gestionando == 1)
+                        self.insertarTablero
+                    elsif(@gestionando == 2)
+                        self.borrarTablero
+                    elsif(@gestionando == 3)
+                        self.asociarCasilla
+                    elsif(@gestionando == 4)
+                        self.verTablero
+                    end
+
                 end
-                gestionando = "vamos"
+                @gestionando = "OK"
             elsif(gestionar == 2)
-                while gestionando != "quit" do
+                while @gestionando != "quit" do
                     puts "Gestión de Casillas"
                     puts "\t1- Insertar \n\t2- Borrar \n\t3- Modificar"
                     print "Opcion: "
-                    gestionando = gets.chomp
+                    @gestionando = Integer(gets.chomp)
                 end
-                gestionando = "vamos"
+                @gestionando = "OK"
             else
-                while gestionando != "quit" do
+                while @gestionando != "quit" do
                     puts "Gestión de Tarjetas"
                     puts "\t1- Insertar \n\t2- Borrar \n\t3- Modificar"
                     print "Opcion: "
-                    gestionando = gets.chomp
+                    @gestionando = Integer(gets.chomp)
                 end
-                gestionando = "vamos"
+                @gestionando = "OK"
             end
         end
+    end
+end
+
+begin
+    con = Mysql.new 'localhost', 'monopoly', 'monopoly', 'Monopoly'
+
+    puts "En caso de ser un Administrador escriba 1."
+    individuo = Integer(gets.chomp)
+    if(individuo == 1)
+        admin = Administrador.new("admin")
+        admin.gestion        
     else
         print "Usuario: "
         usuario = gets.chomp
