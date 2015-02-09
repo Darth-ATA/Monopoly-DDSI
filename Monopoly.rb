ea888db73986365c97e549893c32bcf52e06113a
Darth-ATA <darthata@gmail.com>
2015-02-09 10:36:20 +0100

    He cambiado el usuario la contraseña, para ocultar información personal

---
+++ Monopoly.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/irb
+require 'mysql'
+
+class Administrador
+    #Inserción de un tablero en la base de datos
+    def insertarTablero(id_tablero)
+        con.query("INSTERT INTO Tablero(idTablero) VALUES('#{id_tablero}')")
+    end
+    def borrarTablero(id_tablero)
+        con.query("DELETE FROM Tablero")
+    end
+end
+begin
+    con = Mysql.new 'localhost', 'monopoly', 'monopoly', 'Monopoly'
+    
+    gestionando = "vamos"
+    subsistema = "vamos"
+
+    puts "En caso de ser un Administrador escriba 1."
+    individuo = Integer(gets.chomp)
+    if(individuo == 1)
+        while(subsistema != "quit")
+            puts "Bienvenido Administrador"
+            puts "¿Qué desea gestionar? \n\t1- Tablero \n\t2- Casillas \n\t3- Tarjetas"
+            print "Opcion: "
+            gestionar = Integer(gets.chomp)
+            if(gestionar == 1)
+                while gestionando != "quit" do
+                    puts "Gestión de Tableros"
+                    puts "\t1- Insertar \n\t2- Borrar \n\t3- Asociar una casilla a un tablero"
+                    print "Opcion: "
+                    gestionando = gets.chomp
+                end
+                gestionando = "vamos"
+            elsif(gestionar == 2)
+                while gestionando != "quit" do
+                    puts "Gestión de Casillas"
+                    puts "\t1- Insertar \n\t2- Borrar \n\t3- Modificar"
+                    print "Opcion: "
+                    gestionando = gets.chomp
+                end
+                gestionando = "vamos"
+            else
+                while gestionando != "quit" do
+                    puts "Gestión de Tarjetas"
+                    puts "\t1- Insertar \n\t2- Borrar \n\t3- Modificar"
+                    print "Opcion: "
+                    gestionando = gets.chomp
+                end
+                gestionando = "vamos"
+            end
+        end
+    else
+        print "Usuario: "
+        usuario = gets.chomp
+        print "Contraseña: "
+        contraseña = gets.chomp
+        puts "................."
+        puts "Bienvenido a Monopoly #{usuario}"
+    end
+
+    
+rescue Mysql::Error => e
+    puts e.errno
+    puts e.error
+    
+ensure
+    con.close if con
+end
